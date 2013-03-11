88e022a11122b429cff9d98ee455c37454257641
Ryan Greenberg <ryangreenberg@gmail.com>
2013-03-11 14:31:33 -0700

    Add details to powerset implementation

--- ruby/powerset.rb
+++ ruby/powerset.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby -wKU
 
+# The powerset of a set is the set of all combinations
+#
+# Start with the empty list. For each element add it to
+# all existing members of the list
 def powerset(*args)
   args.inject([[]]) do |arr, i|
     arr.concat(arr.map {|ea| ea + [i]})
   end
 end
 
-puts powerset(1,2,3,4,5).inspect
+puts powerset(1, 2, 3).inspect
+# => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
