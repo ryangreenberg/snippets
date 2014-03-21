927f65ffbf9e25763bd36e419e4c7e787550a8f5
Ryan Greenberg <ryangreenberg@gmail.com>
2014-03-21 13:43:12 -0700

    Explain drawback to longest common prefix solution

--- scala/longest_common_prefix.scala
+++ scala/longest_common_prefix.scala
@@ -1,3 +1,7 @@
+// This could be done without .zip if I could think of a way to use the
+// takeWhile iterator with an index to access str2.
+//
+// There may also be a way to make zip lazy, in which case it's fine
 def longestCommonPrefix(str1: String, str2: String): String = {
   str1.zip(str2).takeWhile { case (c1, c2) => c1 == c2 }.map { case (c1, c2) => c1 }.mkString
 }
